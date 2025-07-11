42d2552bf2523a0e86965a35d6639a3cd9e4a734
Arshia Pourtahmasbi <arshiapourtahmasbi@gmail.com>
2025-07-11 20:00:35 +0330

    Made user system

--- Educationalportal/portal/urls.py
+++ Educationalportal/portal/urls.py
@@ -4,4 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     # Add more paths for other views as needed
+    path('signup/', views.signup, name='signup'),
+    # This will handle the signup view
+    path('login/', views.login_view, name='login'),
+
+    path('logout/', views.logout_view, name='logout'),
+
 ]
--- Educationalportal/portal/views.py
+++ Educationalportal/portal/views.py
@@ -1,5 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import login
+from .forms import SignUpForm
+from .forms import LoginForm
+from django.contrib.auth import authenticate, login, logout
+
 
 def home(request):
-    return HttpResponse("Welcome to the Educational Portal!")
+    return render(request, 'home.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid(): 
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:   
+                login(request, user)
+            return redirect('home')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
