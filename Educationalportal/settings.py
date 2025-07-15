aa8a30dad5e766001980c4ea4269e1cb5596ab2b
Arshia Pourtahmasbi <arshiapourtahmasbi@gmail.com>
2025-07-15 22:51:28 +0330

    Added basic static files

--- Educationalportal/settings.py
+++ Educationalportal/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles',  
     'portal',  # Custom app for the educational portal
     'course',  # App for course management
     'student',  # App for student management
@@ -120,6 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
